d8d695ff0a1b96cfae3237eb4e388dfc76d5d3a3
Eric Swanson <es20641@gmail.com>
2020-10-03 13:25:38 -0500

    First part

---
+++ api/app.js
@@ -0,0 +1,15 @@
+require("express");
+
+const app = express();
+
+const pathToIndex = path.resolve(__dirname, "../client/index.html");
+
+app.use(
+  "/*",
+  (request,
+  (response) => {
+    response.sendFile(pathToIndex);
+  })
+);
+
+module.exports = app;
